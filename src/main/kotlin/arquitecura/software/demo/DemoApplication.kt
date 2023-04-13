3c5f98b6187237931a87c5fc2de7344b9d08d274
Amir Rojas <amir.rojas@ucb.edu.bo>
2023-04-12 23:47:00 -0400

    Agregando bootstrap porperties

--- src/main/kotlin/arquitecura/software/demo/DemoApplication.kt
+++ src/main/kotlin/arquitecura/software/demo/DemoApplication.kt
@@ -2,8 +2,10 @@ package arquitecura.software.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class DemoApplication
 
 fun main(args: Array<String>) {
